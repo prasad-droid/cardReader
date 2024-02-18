723832fe53e7911a5a760cdee2595214456e7019
prasad <prasadkalvikatti@gmail.com>
2024-02-18 11:09:52 +0530

    added Filter

--- src/screens/Card.js
+++ src/screens/Card.js
@@ -1,14 +1,19 @@
+import React, {useState} from 'react';
 import {
   View,
   Image,
   Text,
   StyleSheet,
   Linking,
+  Share,
   TouchableOpacity,
+  TouchableWithoutFeedback,
 } from 'react-native';
-import Button from './Button';
-
+import Icon from 'react-native-vector-icons/FontAwesome6';
 export default function Card({Cname, email, Contact}) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // function to make call
   const makeCall = number => {
     if (typeof number != 'string') {
       number = String(number);
@@ -20,45 +25,92 @@ export default function Card({Cname, email, Contact}) {
     Linking.openURL(phoneNumber);
   };
 
+  // function to message on WhatsApp
   const whatsappMsg = number => {
-    if (typeof number != 'string') {
-      number = String(number);
-      if (!number.startsWith('+91')) {
-        number = '+91' + number;
-      }
-        const phoneNumber = `whatsapp://send?phone=${number}`;
-        Linking.openURL(phoneNumber);
-        console.log("Error Occured ", err);
-    } else {
-      console.error('Contact number is not a valid string');
+    try {
+      const phoneNumber = `whatsapp://send?phone=${number}`;
+      Linking.openURL(phoneNumber);
+    } catch (err) {
+      alert('Error Occured ', err);
     }
   };
+
+  // Function to send an email
+  const sendEmail = emailAddress => {
+    emailAddress = emailAddress.toLowerCase();
+    const mailtoUrl = `mailto:${emailAddress}`;
+    Linking.openURL(mailtoUrl);
+  };
+
+  // Function to share contacts
+  const shareContact = (Cname, Contact, email) => {
+    Share.share({
+      title: 'Contact Information',
+      message: `Name: ${Cname}\nContact: ${Contact}\nEmail: ${email}`,
+    });
+  };
+
+  const toggleMenu = () => {
+    console.log('clicked');
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  // Editing Contact
+  const EditContact = () => {
+    
+  };
+
   return (
-    <View style={styles.card}>
-      <Image
-        source={require('../assets/Profileicon.png')}
-        style={styles.image}
-      />
-      <View style={styles.content}>
-        <Text style={styles.text}>Name : {Cname}</Text>
-        <Text style={styles.text}>Contact : {Contact}</Text>
-        <Text style={styles.text}>Email : {email}</Text>
+    <TouchableWithoutFeedback onPress={closeMenu}>
+      <View style={styles.card}>
+        <View style={styles.contentContainer}>
+          <Image
+            source={require('../assets/Profileicon.png')}
+            style={styles.image}
+          />
+          <View style={styles.content}>
+            <Text style={styles.text}>Name: {Cname}</Text>
+            <Text style={styles.text}>Contact: {Contact}</Text>
+            <Text style={styles.text}>Email: {email}</Text>
+          </View>
+          <TouchableOpacity onPress={toggleMenu}>
+            <Icon
+              name={'ellipsis-vertical'}
+              style={{fontSize: 18, padding: 10}}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              EditContact();
+            }}>
+            <Icon name={'pen'} style={{fontSize: 18, padding: 10}} />
+          </TouchableOpacity>
+          {menuOpen && (
+            <View style={styles.menu}>
+              <TouchableOpacity onPress={() => makeCall(Contact)}>
+                <Text style={styles.menuItem}>Call</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => whatsappMsg(Contact)}>
+                <Text style={styles.menuItem}>WhatsApp</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={() => sendEmail(email)}>
+                <Text style={styles.menuItem}>Email</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => shareContact(Cname, Contact, email)}>
+                <Text style={styles.menuItem}>Share</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
       </View>
-      <View style={styles.btnDiv}>
-        <Button
-          icon={'phone'}
-          onPress={() => {
-            makeCall(Contact);
-          }}
-        />
-        <Button
-          icon="whatsapp"
-          onPress={() => {
-            whatsappMsg(Contact);
-          }}
-        />
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -68,13 +120,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#eee',
     margin: 10,
     borderRadius: 10,
+    flexDirection: 'column',
+    position: 'relative',
+    elevation: -2,
+    zIndex: -10,
+  },
+  contentContainer: {
     flexDirection: 'row',
-    // height:100
   },
   image: {
     width: 80,
     height: 80,
-    // backgroundColor:'#f00',
     borderRadius: 10,
   },
   content: {
@@ -82,11 +138,19 @@ const styles = StyleSheet.create({
     padding: 10,
     width: 200,
   },
-  btnDiv: {
-    padding: 0,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    // alignItems:"center"
+  menu: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 5,
+    elevation: 5,
+    zIndex: 5,
+  },
+  menuItem: {
+    marginBottom: 5,
+    color: '#333',
   },
   text: {
     color: '#000',
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,47 +1,76 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import {
   View,
   Text,
   StyleSheet,
   ScrollView,
   ImageBackground,
+  TextInput,
   SafeAreaView,
 } from 'react-native';
-import { doc, getDoc } from 'firebase/firestore';
-import { DataContext } from './Context';
+import Button from './Button';
+import {doc, getDoc, getDocFromCache} from 'firebase/firestore';
+import {DataContext} from './Context';
 import Card from './Card';
-import { db } from '../firebaseConfig';
+import {db} from '../firebaseConfig';
 
-export default  Home = () => {
+export default Home = () => {
   const user = useContext(DataContext);
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const docRef = doc(db, 'users', user.email);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const allContacts = docSnap.data().contacts || [];
+        console.log('Docs  :' + JSON.stringify(allContacts));
+        setFilteredData(allContacts);
+      } else {
+        console.log('No such document!');
+        setError('No data found in Firestore');
+      }
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setError('Error fetching data from Firestore');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const docRef = doc(db, 'users', user.email);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          console.log('Document data:', docSnap.data());
-          setData(docSnap.data());
-        } else {
-          console.log('No such document!');
-          setError('No data found in Firestore');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setError('Error fetching data from Firestore');
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchData();
   }, []);
 
+  // function to handle Refresh
+  const handleRefresh = () => {
+    fetchData();
+  };
+
+  // function to handle SearchInput
+  const handleSearch = () => {
+    const searchTerm = searchInput.toLowerCase();
+    if(searchTerm.length == 0){
+      fetchData();
+    }else{
+      const filtered = filteredData.filter(contact => {
+        return (
+          contact.name.toLowerCase().includes(searchTerm) ||
+          contact.email.toLowerCase().includes(searchTerm) ||
+          contact.contact1.toLowerCase().includes(searchTerm)
+          );
+        });
+        setFilteredData(filtered);
+      }
+
+  };
+
+  const handleFilter = () => {};
   return (
     <SafeAreaView style={styles.main}>
       <View style={styles.container}>
@@ -49,14 +78,30 @@ export default  Home = () => {
           source={require('../assets/appicon.png')}
           resizeMode="contain"
           style={styles.image}>
+          <View style={styles.topNav}>
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Search..."
+              value={searchInput}
+              onChangeText={text => setSearchInput(text)}
+              onSubmitEditing={handleSearch}
+            />
+            <Button icon="arrows-rotate" onPress={handleRefresh}></Button>
+            <Button icon="filter" onPress={handleFilter} />
+          </View>
           {loading ? (
             <Text style={styles.emptyText}>Loading...</Text>
           ) : error ? (
             <Text style={styles.errorText}>Error: {error}</Text>
-          ) : data && data.contacts && data.contacts.length > 0 ? (
+          ) : filteredData.length > 0 ? (
             <ScrollView style={styles.scrollView} vertical={true}>
-              {data.contacts.map((contact, index) => (
-                <Card key={index} Cname={contact.name} email={contact.email} Contact={contact.contact1}  />
+              {filteredData.map((contact, index) => (
+                <Card
+                  key={index}
+                  Cname={contact.name}
+                  email={contact.email}
+                  Contact={contact.contact1}
+                />
               ))}
             </ScrollView>
           ) : (
@@ -83,10 +128,10 @@ const styles = StyleSheet.create({
   },
   emptyText: {
     color: 'black',
-    backgroundColor:'#0004',
+    backgroundColor: '#0004',
     fontSize: 18,
     textAlign: 'center',
-    padding:10,
+    padding: 10,
     marginTop: 20,
   },
   errorText: {
@@ -98,5 +143,17 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     justifyContent: 'center',
+  },
+  topNav: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  searchInput: {
+    color: 'black',
+    width: 250,
+    backgroundColor: 'white',
+    borderRadius: 12,
   },
 });
